8bf926ef25a1eb3035a56ab3ffac9817b1dc287f
Julius Krause <julius.krause@scandio.de>
2024-06-26 09:40:10 +0200

    center cards for bigger screens

--- src/App.style.ts
+++ src/App.style.ts
@@ -1,11 +1,15 @@
 import { styled } from '@mui/material';
+import { breakpointValues } from './theme.ts';
 
-export const PageContainerStyled = styled('div')(() => ({
+export const PageContainerStyled = styled('div')(({ theme }) => ({
   margin: 0,
   padding: 0,
   display: 'inline-flex',
   justifyContent: 'center',
   alignItems: 'flex-start',
   minHeight: '100vh',
-  minWidth: '100vw'
+  minWidth: '100vw',
+  [theme.breakpoints.up(breakpointValues.sm)]: {
+    alignItems: 'center'
+  }
 }));
